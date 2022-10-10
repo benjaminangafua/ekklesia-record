d346a9c487c25a45cf45ac1ab4a41ca004fc21cc
benjaminangafua1997 <benjaminarkutl2017@gmail.com>
2022-10-10 17:23:41 +0000

    Updated .env file

--- churchAPP/__init__.py
+++ churchAPP/__init__.py
@@ -1,25 +1,29 @@
 from datetime import timedelta
+from distutils.command.config import config
 from flask import Flask
 from flask_session import Session
 from flask_mail import Mail, Message
 from cs50 import SQL
 from tempfile import mkdtemp
+from dotenv import dotenv_values
+
 db = SQL('sqlite:///church.db')
 
 def create_app():
     app = Flask(__name__)
-    mail = Mail(app)
-    app.secret_key = 'SomeKinda secret key'
+
+    config = dotenv_values('.env')
+    app.secret_key = config["SECRET_KEY"]
     app.config['SESSION_TYPE'] = 'filesystem'
     app.config["SESSION_FILE_DIR"] = mkdtemp()
     app.config["SESSION_PERMANENT"] = True
     app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(days=3)
     Session(app)
 
-    app.config['MAIL_SERVER']='smtp.gmail.com'
-    app.config['MAIL_PORT'] = 587
-    app.config['MAIL_USERNAME'] = 'benjaminarkutl2017'
-    app.config['MAIL_PASSWORD'] = 'aeszorrkndccadsm'
+    app.config['MAIL_SERVER']= config['MAIL_SERVER']
+    app.config['MAIL_PORT'] = config['MAIL_PORT']
+    app.config['MAIL_USERNAME'] = config['MAIL_USERNAME']
+    app.config['MAIL_PASSWORD'] = config['MAIL_PASSWORD']
     app.config['MAIL_USE_TLS'] = True
     # app.config["MAIL_DEFAULT_SENDER"] = "benjaminarkutl2017@gmail.com"
     mail = Mail(app)
